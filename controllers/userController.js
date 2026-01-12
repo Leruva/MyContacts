9672ca10478306b16d8b00be7c306bf23d0bff6f
Leruva <sjruva2006@gmail.com>
2026-01-12 22:27:49 +0530

    user password and registration

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,36 @@
 const asyncHandler = require("express-async-handler");
-
+const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
 //@desc register user
 //@route POST /api/users/register
 //@access public
 
 const registerUser = asyncHandler(async(req,res)=>{
+    const {email , password,username} = req.body;
+    if(!email || !password || !username){
+        res.status(400);
+        throw new Error("All fields are mandatory");
+    }
+    const userAvailable = await User.findOne({email});
+    if(userAvailable) {
+        res.status(400);
+        throw new Error("User already registered");
+    }
+    //hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+    console.log("hashedPassword : ",hashedPassword);
+
+    const user = await User.create({
+        username,
+        email,
+        password: hashedPassword,
+    });
+    if (user){
+        res.status(201).json({_id: user.id, email: user.email});
+    }else{
+        res.status(400);
+        throw new Error("User data is not valid");
+    }
     res.json({message: "Register the user"})
 });
 //@desc register user
@@ -12,6 +38,8 @@ const registerUser = asyncHandler(async(req,res)=>{
 //@access public
 
 const loginUser = asyncHandler(async(req,res)=>{
+
+
     res.json({message: "login user"})
 });
 //@desc register user
